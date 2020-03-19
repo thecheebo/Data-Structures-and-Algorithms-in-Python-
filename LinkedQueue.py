ad8d7e848362a0f0c8125ae36ec2050a5f5425eb
thecheebo <59412572+thecheebo@users.noreply.github.com>
2020-03-19 14:43:23 -0700

    Update LinkedQueue.py

--- LinkedQueue.py
+++ LinkedQueue.py
@@ -79,17 +79,17 @@ class LinkedQueue:
         
         if self.is_empty():
             raise Empty('Queue is empty')
-        answer = self._head._element
+        answer = self._head.data
         self._head = self._head._next
         self._size -= 1
-        if self._empty():
+        if self.is_empty():
             self._tail = None
         return answer
         
-    def enqueue(self):
+    def enqueue(self, element):
         """Add an element to the back of the queue (FIFO)"""
         
-        newest = self._Node(e, None)
+        newest = self._Node(element, None)
         if self.is_empty():
             self._head = newest
         else:
@@ -97,5 +97,10 @@ class LinkedQueue:
         self._tail = newest
         self._size +=1
 
-
+a = LinkedQueue()
+a.enqueue(5)
+a.enqueue(10)
+a.enqueue(7)
+a.enqueue(9)
+a.dequeue()
 
