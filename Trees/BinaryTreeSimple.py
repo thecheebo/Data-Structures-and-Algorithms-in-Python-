646b8a9f6e25b103f78a4b8f40df8552b26454f2
thecheebo <59412572+thecheebo@users.noreply.github.com>
2020-03-18 20:37:57 -0700

    Update BinaryTreeSimple.py

--- Trees/BinaryTreeSimple.py
+++ Trees/BinaryTreeSimple.py
@@ -1,5 +1,29 @@
 # Binary Search Tree in Python
 
+class Queue(object):
+	def __init__(self):
+		self.items = []
+	
+	def enqueue(self, item):
+		self.items.insert(0, item)
+		
+	def dequeue(self):
+		if not self.is_empty():
+			return self.items.pop()
+	
+	def is_empty(self):
+		return len(self.items) == 0
+	
+	def peek(self):
+		if not self.is_empty():
+			return self.items[-1].value
+		
+	def	__len__(self):
+		return self.size()
+	
+	def size(self):
+		return len(self.items)
+
 class Node:
 	def __init__(self, val):
 		self.value = val
@@ -202,6 +226,23 @@ class Tree:
 		if self.root is not None:
 			print("InOrder")
 			self.root.inorder()
+	
+	def levelorder_print(self, start): #breadthfirst- traversal
+		if start is None:
+			return
+		queue = Queue()
+		queue.enqueue(start)
+		
+		traversal = ""
+		while len(queue) > 0:
+			traversal += str(queue.peek()) + " - "
+			node = queue.dequeue()
+			
+			if node.left:
+				queue.enqueue(node.left)
+			if node.right:
+				queue.enqueue(node.right)
+		return traversal
 
 bst = Tree()
 print(bst.insert(10))
