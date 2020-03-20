73912350a9a5f0ed32b15e736ba9a2883a574f0d
thecheebo <59412572+thecheebo@users.noreply.github.com>
2020-03-20 16:44:31 -0700

    Update LinkedBinaryTree.py

--- Trees/LinkedBinaryTree.py
+++ Trees/LinkedBinaryTree.py
@@ -19,68 +19,68 @@ class Tree:
             
         # ---------- abstract methods that concrete subclass must support ----------  
        
-        def root(self):
-            """Return Position representing the tree s root (or None if empty)."""
-            raise NotImplementedError('must be implemented by subclass')
-            
-        def parent(self, p):
-            """Return Position representing p s parent (or None if p is root)."""
-            raise NotImplementedError('must be implemented by subclass')
-        
-        def num_children(self, p):
-            """Return the number of children that Position p has."""
-            raise NotImplementedError('must be implemented by subclass')
-            
-        def children(self, p):
-            """Generate an iteration of Positions representing p s children."""
-            raise NotImplementedError('must be implemented by subclass')
-            
-        def len (self):
-            """Return the total number of elements in the tree."""
-            raise NotImplementedError('must be implemented by subclass')
-            
-        # ---------- concrete methods implemented in this class ----------
-        
-        def is_root(self, p):
-            """Return True if Position P represents the root of the tree."""
-            return self.root()==p
-        
-        def is_leaf(self, p):
-            """Return true if Position p does not have any children."""
-            return self.num_children(p)== 0
-            
-        def is_empty(self):
-            """Return True if the tree is empty."""
-            return len(self)== 0
-        
-        def depth(self, p):
-            """Return the number of levels separating Position p from the root."""
-            if self.is_root(p):
-                return 0
-            else:
-                return 1 + self.depth(self.parent(p))
-                
-        def _height1(self):
-            """Return the height of the tree"""
-            return max(self.depth(p) for p in self.positions() if self.is_leaf(p))
-            
-        def _height2(self, p):
-            """"Return the heightof the subtree rooted at Position p"""
-            if self.is_leaf(p):
-                return 0
-            else:
-                return 1 +max(self.height2(c) for c in self.children(p))
-                    
-        def height(self, p=None):
-            """Return the heigh of subtree rooted at Position P.  If P is none, return the height of the entire tree."""
-            if p is None:
-                p = self.root()
-            return self.height2(p)
-        
-        def __iter__(self):
-            """Generate an iteration of the tree's elements."""
-            for p in self. positions():
-                yield p.element()
+    def root(self):
+        """Return Position representing the tree s root (or None if empty)."""
+        raise NotImplementedError('must be implemented by subclass')
+
+    def parent(self, p):
+        """Return Position representing p s parent (or None if p is root)."""
+        raise NotImplementedError('must be implemented by subclass')
+
+    def num_children(self, p):
+        """Return the number of children that Position p has."""
+        raise NotImplementedError('must be implemented by subclass')
+
+    def children(self, p):
+        """Generate an iteration of Positions representing p s children."""
+        raise NotImplementedError('must be implemented by subclass')
+
+    def len (self):
+        """Return the total number of elements in the tree."""
+        raise NotImplementedError('must be implemented by subclass')
+
+    # ---------- concrete methods implemented in this class ----------
+
+    def is_root(self, p):
+        """Return True if Position P represents the root of the tree."""
+        return self.root()==p
+
+    def is_leaf(self, p):
+        """Return true if Position p does not have any children."""
+        return self.num_children(p)== 0
+
+    def is_empty(self):
+        """Return True if the tree is empty."""
+        return len(self)== 0
+
+    def depth(self, p):
+        """Return the number of levels separating Position p from the root."""
+        if self.is_root(p):
+            return 0
+        else:
+            return 1 + self.depth(self.parent(p))
+
+    def _height1(self):
+        """Return the height of the tree"""
+        return max(self.depth(p) for p in self.positions() if self.is_leaf(p))
+
+    def _height2(self, p):
+        """"Return the heightof the subtree rooted at Position p"""
+        if self.is_leaf(p):
+            return 0
+        else:
+            return 1 +max(self.height2(c) for c in self.children(p))
+
+    def height(self, p=None):
+        """Return the heigh of subtree rooted at Position P.  If P is none, return the height of the entire tree."""
+        if p is None:
+            p = self.root()
+        return self.height2(p)
+
+    def __iter__(self):
+        """Generate an iteration of the tree's elements."""
+        for p in self. positions():
+            yield p.element()
         
 
 class BinaryTree(Tree):
