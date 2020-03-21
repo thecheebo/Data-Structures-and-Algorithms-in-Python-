b3af676c3493756d90b83237b93f6113aaf4c087
thecheebo <59412572+thecheebo@users.noreply.github.com>
2020-03-20 18:03:58 -0700

    Create TestBinaryTree.py

---
+++ Trees/TestBinaryTree.py
@@ -0,0 +1,25 @@
+Class Tree:
+    class Position:
+        
+        def element(self):
+        
+        def __eq__(self, other):
+        
+        def __ne__(self, other):
+    
+    def root(self):
+    
+    def parent(self):
+    
+    def num_children(self): 
+    
+    def children(self):
+    
+    def __len__(self):
+    
+    def is_root(self, p):
+        return self.root() == p
+class BinaryTree(Tree):
+
+
+class LinkedBinaryTree(BinaryTree):
