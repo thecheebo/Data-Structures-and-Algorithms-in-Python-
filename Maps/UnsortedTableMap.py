2957b1e4b7a897a5f69fea0cc6df2cf3b0fbbcaf
thecheebo <59412572+thecheebo@users.noreply.github.com>
2020-03-20 21:33:26 -0700

    Update and rename UnsortedTableMap to UnsortedTableMap.py

---
+++ Maps/UnsortedTableMap.py
@@ -0,0 +1,16 @@
+class UnsortedTableMap(MapBase):
+    """Map implementation using an unordered list."""
+    
+    def __init__(self):
+        """Create an empty map."""
+        self._table = []                #list of _Item's
+    
+    def __getitem__(self, k)
+        for item in self._table:
+            if k == item._key:
+                return item._value
+        raise KeyError('Key Error: " + repr(k))
+    
+    def __setitem__(self, k, v):
+        """assign value v to key k, overwriting existing value if present"""
+
