2947bb44819edd75edca09ce5bede1af7e46c2b7
thecheebo <59412572+thecheebo@users.noreply.github.com>
2020-03-23 15:19:58 -0700

    Create HashMapChainingSimple.py

---
+++ Maps/HashMapChainingSimple.py
@@ -0,0 +1,11 @@
+INITIAL_CAPACITY = 50
+
+class Node:
+    def  __init(self, key, value)
+        self.key = key
+        self.value = value
+        self.next = None
+        
+class HashTable:
+    def __init__(self):
+    
