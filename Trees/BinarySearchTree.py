5e06e8d0a5d4fe015ed86cd9c1233c656006abb1
thecheebo <59412572+thecheebo@users.noreply.github.com>
2020-03-24 18:36:30 -0700

    Update BinarySearchTree.py

--- Trees/BinarySearchTree.py
+++ Trees/BinarySearchTree.py
@@ -4,22 +4,34 @@ class Node:
         self.leftChild = None
         self.rightChild = None
         
-        def insert(self, data):
-            if self.value == data:
-                return False
-            elif self.value > data:
-                if self.leftChild:
-                    return self.leftChild.insert(data)
-                else:
-                    self.leftChild = Node(data)
-                    return True
+    def insert(self, data):
+        if self.value == data:
+            return False
+        elif self.value > data:
+            if self.leftChild:
+                return self.leftChild.insert(data)
             else:
-                if self.rightChild:
-                    return self.rightChildinsert(data)
-                else:
-                    self.rightChild = Node(data)
-                    return True
-               
+                self.leftChild = Node(data)
+                return True
+        else:
+            if self.rightChild:
+                return self.rightChildinsert(data)
+            else:
+                self.rightChild = Node(data)
+                return True
+    def find(self, data):
+        if(self.value == data):
+            return True
+        elif self.value > data:
+            if self.leftChild:
+                return self.leftChild.find(data)
+            else:
+                return False
+        else:
+            if self.rightChild:
+                return self.rightChild.find(data)
+            else:
+                return False
 
 class Tree:
     def __init__(self):
@@ -31,3 +43,10 @@ class Tree:
         else:
             self.root = Node(data)
             return True
+
+    def find(self, data):
+        if self.root:
+            return self.root.find(data)
+        else:
+            return False
+    
